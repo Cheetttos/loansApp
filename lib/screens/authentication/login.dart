a44b19fd920aa50a6333dc64cf4f877258e5d25d
Destiny Ed <destinyed@Destinys-MacBook-Pro.local>
2024-01-15 15:09:57 +0100

    completed login screen

--- lib/screens/authentication/login.dart
+++ lib/screens/authentication/login.dart
@@ -1,6 +1,7 @@
 import 'package:expense_tracker/config/extensions.dart';
 import 'package:expense_tracker/enums/enums.dart';
 import 'package:expense_tracker/provider/authentication/auth_provider.dart';
+import 'package:expense_tracker/shared/utils/message.dart';
 import 'package:expense_tracker/shared/widgets/busy_overlay.dart';
 import 'package:expense_tracker/shared/widgets/custom_button.dart';
 import 'package:expense_tracker/styles/color.dart';
@@ -8,6 +9,7 @@ import 'package:expense_tracker/styles/theme.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_text_form_field/flutter_text_form_field.dart';
+import 'package:flutter_utilities/flutter_utilities.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
@@ -24,6 +26,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return Consumer<AuthenticationProviderImpl>(builder: (context, stateModel, child) {
       return BusyOverlay(
         show: stateModel.state == ViewState.Busy,
+        title: stateModel.message,
         child: Scaffold(
           body: Padding(
             padding: const EdgeInsets.all(20),
@@ -38,23 +41,45 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     120.height(),
                     CustomTextField(
-                      TextEditingController(),
+                      stateModel.emailController,
                       hint: 'Email',
                       password: false,
                       border: Border.all(color: greyColor),
                     ),
                     20.height(),
                     CustomTextField(
-                      TextEditingController(),
+                      stateModel.passwordController,
                       hint: 'Password',
                       password: true,
                       border: Border.all(color: greyColor),
                     ),
                     100.height(),
                     CustomButton(
-                      onPressed: () {
-                        print('hello ');
-                        context.go('/loan_dashboard');
+                      onPressed: () async {
+                        if (stateModel.userNameController.text.isEmpty ||
+                            stateModel.passwordController.text.isEmpty) {
+                          showMessage(context, 'All fields are required', isError: true);
+                          return;
+                        }
+                        if (!FlutterUtilities().isEmailValid(stateModel.emailController.text.trim())) {
+                          showMessage(context, "Invalid email provided", isError: true);
+                          return;
+                        }
+
+                        await stateModel.loginUser();
+
+                        if (stateModel.state == ViewState.Error) {
+                          if (context.mounted) {
+                            showMessage(context, stateModel.message, isError: true);
+                          }
+                          return;
+                        }
+                        if (stateModel.state == ViewState.Success) {
+                          if (context.mounted) {
+                            showMessage(context, stateModel.message);
+                            context.go('/loan_dashboard');
+                          }
+                        }
                       },
                       text: 'Login',
                     ),
